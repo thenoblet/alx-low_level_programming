259e4801da58c254d4f31aae84bbf49f2978e615
noblet <appiahpatricknoblet@gmail.com>
2023-10-06 01:53:29 +0100

    printf function in action

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entry point
- * 
+ *
  * Return: Always 0 (success)
  */
 
@@ -10,4 +10,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-} 
+}
