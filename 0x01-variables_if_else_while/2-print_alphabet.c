db0a8dc5a0dc9d307fb82bc11fb47f270e3b3a6f
noblet <appiahpatricknoblet@gmail.com>
2023-10-07 02:44:54 +0100

    a program that prints the alphabet in lowercase except q and e

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,6 +3,8 @@
 /**
  * main - entry point
  *
+ * Program that prints the alphabet in lowercase except q and e"
+ *
  * Return: Always 1 (Success)
  *
  * Lower level programming - 0x01. C - Variables, if, else, while
