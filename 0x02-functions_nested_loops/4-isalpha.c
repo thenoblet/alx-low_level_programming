72adc50dc2d2ba3f2e6978399e6529c92a4e2f61
noblet <appiahpatricknoblet@gmail.com>
2023-10-11 00:30:34 +0100

    added a function that checks for alphabetic chars

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,21 +3,19 @@
 /**
  * _isalpha - checks for alphabetic character
  *
+ * @c: parameter value
+ *
  * Return: Returns 1 if c is a letter, lowercase or uppercase.
  *
  */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-	else if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else 
+	else
 	{
 		return (0);
 	}
