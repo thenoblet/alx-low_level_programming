21430fe9103ab8c8a14e6e24f641df8d3d359aa8
noblet <appiahpatricknoblet@gmail.com>
2023-10-12 21:47:56 +0100

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9,
+ * except 2 and 4
+ *
+ * Return: viod (success)
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 9; num++)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,4 +14,6 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+void print_most_numbers(void);
+
 #endif /* MAIN_H */
