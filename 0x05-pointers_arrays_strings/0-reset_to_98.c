bf4988fefd1350f3c1a443cc4db9df6eaed11d9a
noblet <appiahpatricknoblet@gmail.com>
2023-10-17 00:10:22 +0100

    added README file, removed trailing white spaces

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * 
+ *
  * @n: parameter
  *
  * Return: Void
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# Low level programming
+
+0x05. C - Pointers, arrays and strings
+
