6004058186940d956e5ee54e66fb79e150717c19
noblet <appiahpatricknoblet@gmail.com>
2023-10-19 20:28:41 +0100

    added main.h 0-strcat.c and README files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate strins
+ *
+ * @dest: destination string
+ * @src: string to append (source string)
+ *
+ * Return: destination string
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int destLen = 0;
+	int i = 0;
+
+	while (dest[destLen] != '\0')
+	{
+		destLen++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[destLen++] = src[i];
+		i++;
+	}
+	dest[destLen] = '\0';
+
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# C - low level programming
+
+
+0x06. C - More pointers, arrays and strings
