fff40957e0f7dfdb566641cd3b6cd31cd3112029
noblet <appiahpatricknoblet@gmail.com>
2023-10-23 18:51:13 +0100

    added a  function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (&s[i]);
+			return (s);
 		}
-		i++;
+		s++;
 	}
 
 	return (NULL);
