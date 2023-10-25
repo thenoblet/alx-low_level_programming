341d408028169f6ff3008a6772b2bd98c5c29a7e
noblet <appiahpatricknoblet@gmail.com>
2023-10-25 23:41:15 +0100

    added a strlen function

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - finds the length of a string
+ *
+ * @s: string
+ *
+ * Return: the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
