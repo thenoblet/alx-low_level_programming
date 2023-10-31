d2159e0d296ae0ed397babb0d8a8f26bb8746d19
noblet <appiahpatricknoblet@gmail.com>
2023-10-31 22:41:25 +0000

    added function that frees dynamically alloc'ed memory

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * free_grid - frees memory for a 2D grid
+ *
+ * @grid: pointer to 2D array
+ * @height: height of the array
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,4 +11,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
+
 #endif /* MAIN_H */
