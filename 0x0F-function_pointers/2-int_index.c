e9da1e606477855e442688bb0fed0ccd603f8423
noblet <appiahpatricknoblet@gmail.com>
2023-11-09 02:50:50 +0000

    added a function that searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -24,16 +24,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
-
-	if (array != NULL || cmp != NULL)
+	if (size || array != NULL || cmp != NULL)
 	{
 		for (i = 0; i < size; ++i)
 		{
 			if (cmp(array[i]))
+				/* Return the index of the first matching element */
 				return (i);
 		}
 	}
