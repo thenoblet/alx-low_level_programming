cf287fceade311d1ef8826eb90fb3a1200340b2c
noblet <appiahpatricknoblet@gmail.com>
2023-11-22 00:23:46 +0000

    Fix: handled case where head pointer is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i, Nth = idx - 1;
 
 	newNode = malloc(sizeof(listint_t));
-	if (newNode == NULL)
+	if (newNode == NULL || head == NULL)
 	{
 		return (NULL);
 	}
