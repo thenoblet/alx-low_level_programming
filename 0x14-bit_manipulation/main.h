d867ca5198a2accd00ecf55f2670d887c8d05928
noblet <appiahpatricknoblet@gmail.com>
2023-11-23 22:12:00 +0000

    added main.h file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define is_binary(c) ((c) == '0' || (c) == '1')
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
