b8b7bc48e5644812fdedd9ee23eb75822437ae6a
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-05-09 01:26:49 +0000

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,6 +11,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
-int recursive_search(int *array, int value, size_t low, size_t high)
+int recursive_search(int *arr, size_t start_index, size_t end_index, int target))
 
 #endif /* SEARCH_ALGOS_H*/
