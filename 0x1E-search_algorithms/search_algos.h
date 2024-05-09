2ceeca775ae3f5a5fb2b6b23221253d12c355a8e
noblet <appiahpatricknoblet@gmail.com>
2024-05-09 02:07:10 +0100

    a function that searches for a value in a sorted array of integers.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int recursive_search(int *array, int left, int right, int value);
 
 #endif /* SEARCH_ALGOS_H*/
